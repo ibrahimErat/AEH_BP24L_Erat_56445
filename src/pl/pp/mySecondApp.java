7ef6932e23cda13cd08f3b6ad852b905125f1f4a
ibrahim erat <errat@ibrahim-MacBook-Pro.local>
2025-03-24 16:23:40 +0300

    Added Task2 and updated mySecondApp with arithmetic operations

---
+++ src/pl/pp/mySecondApp.java
@@ -0,0 +1,33 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mySecondApp {
+    public static void main(String[] args) {
+        double x = 10; // creating a double type variable and assigning it a value of 10
+        double y = 2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result = x + y;
+        System.out.println("x + y = " + result);
+
+        result = x - y;
+        System.out.println("x - y = " + result);
+
+        result = x * y;
+        System.out.println("x * y = " + result);
+
+        result = x / y;
+        System.out.println("x / y = " + result);
+
+        result = x % y;
+        System.out.println("x % y = " + result);
+
+        System.out.println("Enter two numbers separated by the Enter key:");
+        double first = scanner.nextDouble(); // request to enter a double value
+        double second = scanner.nextDouble();
+
+        System.out.println("x + y = " + (first + second));
+    }
+} 
